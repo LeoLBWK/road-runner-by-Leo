c79b7abf5e0235634f2eacabd21ac9a4cbcf4360
LeoLBWK <leo197612@163.com>
2024-12-08 15:47:34 +0800

    Feat:FIXED SOMETHING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/RRTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/RRTest.java
@@ -8,24 +8,3 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
-@Autonomous(group = "Test")
-public class RRTest extends LinearOpMode {
-
-    private MecanumDrive drive;
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(0, 0, 0);
-
-            MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
-
-            waitForStart();
-
-            Actions.runBlocking(
-                    drive.actionBuilder(beginPose)
-                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
-                            .splineTo(new Vector2d(0, 60), Math.PI)
-                            .build()
-            );
-    }
-}
